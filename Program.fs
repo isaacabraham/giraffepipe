5861e3c366773009041027eafac6f0918f29d23f
Isaac Abraham <isaac.abraham@gmail.com>
2021-10-27 23:06:44 +0200

    Merge all demo routes into one fat router

--- Program.fs
+++ Program.fs
@@ -3,6 +3,7 @@ open Giraffe
 open FSharp.Control.Tasks
 open Microsoft.AspNetCore.Http
 
+// Custom http handler / pipeline stage
 let myCustomPipe shouldIDoStuff (next:HttpFunc) (ctx:HttpContext) = task {
     if not shouldIDoStuff then
         return! RequestErrors.unauthorized "SCHEME" "REALM" (text "Go away!") earlyReturn ctx
@@ -11,22 +12,28 @@ let myCustomPipe shouldIDoStuff (next:HttpFunc) (ctx:HttpContext) = task {
         return! next ctx
 }
 
+// Saturn pipeline
 let saturnRouter = router {
-    pipe_through (myCustomPipe false)
+    pipe_through (myCustomPipe false) // compose your custom handler with "the rest of your app"
     get "/" (text "YOU GOT!")
     post "/" (text "YOU POSTED!")
 }
 
 // Giraffe pipeline
 let giraffeRouter : HttpHandler =
-    myCustomPipe true >=>
+    myCustomPipe true >=> // compose your custom handler with "the rest of your app"
     choose [
         GET >=> text "YOU GOT!"
         text "SOME OTHER VERB"
     ]
 
+let appRouter = router {
+    forward "/giraffe" giraffeRouter
+    forward "/saturn" saturnRouter
+}
+
 let app = application {
-    use_router saturnRouter
+    use_router appRouter
 }
 
 run app
